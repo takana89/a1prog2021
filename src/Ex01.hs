06bad1d42663268b94ef0a57f0cc4f1c931838de
Nanaka Takano <btslovesg@gmail.com>
2021-11-16 15:16:47 +0900

    add palindrome and palindromeKai

--- src/Ex01.hs
+++ src/Ex01.hs
@@ -15,5 +15,26 @@ True
 >>> palindrome "abracadabra"
 False
 -}
-palindrome :: String -> Bool
-palindrome str =
+palindrome :: String -> Bool --型シグネチャー
+palindrome str = str == reverse str
+
+{-
+>>> palindromeKai "Madam, I'm Adam."
+True
+
+>>> palindromeKai "たけやぶ　やけた？"
+True
+-}
+palindromeKai :: String -> Bool
+palindromeKai str = palindrome str''
+    where
+        str' = filter isLetter str
+        str'' = map toLower str'
+
+sample1 :: String
+sample1 = "Madam, I'm Adam."
+
+{-
+同値性の検査をする演算子 ==
+-}
+
