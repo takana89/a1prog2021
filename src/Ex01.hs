09e6ab3940b61f8a61abf529bd36125e9f84aba5
Nanaka Takano <btslovesg@gmail.com>
2021-11-18 11:35:00 +0900

    update Ex01 module

--- src/Ex01.hs
+++ src/Ex01.hs
@@ -21,18 +21,42 @@ palindrome str = str == reverse str
 {-
 >>> palindromeKai "Madam, I'm Adam."
 True
-
 >>> palindromeKai "たけやぶ　やけた？"
 True
 -}
 palindromeKai :: String -> Bool
-palindromeKai str = palindrome str''
-    where
-        str' = filter isLetter str
-        str'' = map toLower str'
+-- palindromeKai str = palindrome (map toLowewr (filter isLetter str))
+palindromeKai = palindrome . map toLower . filter isLetter
 
+{-
+h x = g (f x)
+
+        f      g
+    (x) -> (y) -> (z)
+        -------->
+            h
+h は f と g を合成したもの
+h :: x -> z
+f :: x -> y
+g :: y -> z
+
+h = g . f
+-}
 sample1 :: String
 sample1 = "Madam, I'm Adam."
+
+
+otomojidake :: String -> String
+otomojidake = filter isLetter
+
+komojinisuru :: String -> String
+komojinisuru = map toLower
+
+{-
+filter :: (a -> Bool) -> ([a] -> [a])
+map :: (a -> b) -> ([a]-> [b])
+-}
+
 
 {-
 同値性の検査をする演算子 ==
